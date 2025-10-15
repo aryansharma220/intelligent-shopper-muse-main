b19e6670a99496226df4f8cadfde42f0c69ff589
Aryan <aryansharma220318@gmail.com>
2025-10-15 22:40:05 +0530

    path updated

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/",
   server: {
     host: "::",
     port: 8080,
@@ -18,6 +19,7 @@ export default defineConfig({
     outDir: "dist",
     sourcemap: false,
     minify: "esbuild",
+    assetsDir: "assets",
     rollupOptions: {
       output: {
         manualChunks: {
